35300f8dd7348c59106acf047d534c7b8ad15d9f
jctiru <jonathanchristiantiru@gmail.com>
2020-01-30 20:23:19 +1000

    Show loading on register page

--- src/pages/register/registerpage.component.jsx
+++ src/pages/register/registerpage.component.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { createStructuredSelector } from "reselect";
+import LoadingOverlay from "react-loading-overlay";
 
 import {
   selectError,
   selectSignUpSuccessMessage
 } from "../../redux/user/user.selectors";
 import { signUpStart } from "../../redux/user/user.actions";
+import Spinner from "../../components/spinner/spinner.component";
 
 import "./registerpage.styles.scss";
 
@@ -19,8 +21,8 @@ const RegisterPage = ({ error, signUpStart, signUpSuccessMessage }) => {
     password: "",
     confirmPassword: ""
   });
-
   const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     let errorObject = {};
@@ -33,6 +35,10 @@ const RegisterPage = ({ error, signUpStart, signUpSuccessMessage }) => {
 
     setErrors(errorObject);
   }, [error]);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [error, signUpSuccessMessage]);
 
   const {
     firstName,
@@ -50,11 +56,11 @@ const RegisterPage = ({ error, signUpStart, signUpSuccessMessage }) => {
       return;
     }
 
+    setIsLoading(true);
     signUpStart(firstName, lastName, email, password);
   };
 
   const handleChange = event => {
-    console.log("Handle Change", error);
     const { value, name } = event.target;
     setCredentials({ ...userCredentials, [name]: value });
   };
@@ -64,118 +70,120 @@ const RegisterPage = ({ error, signUpStart, signUpSuccessMessage }) => {
       <div className="row">
         <div className="col-md-6 mx-auto">
           {signUpSuccessMessage ? (
-            <div class="alert alert-success">{signUpSuccessMessage}</div>
+            <div className="alert alert-success">{signUpSuccessMessage}</div>
           ) : null}
-          <div className="card border-secondary card-body bg-light mt-5 mb-5">
-            <h2 className="text-dark">Create an Account</h2>
-            <p>Please fill out this form to register with us</p>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="firstName">
-                  First Name: <sup>*</sup>
-                </label>
-                <input
-                  type="text"
-                  name="firstName"
-                  className={`form-control ${
-                    errors.firstName ? "is-invalid" : ""
-                  }`}
-                  value={firstName}
-                  onChange={handleChange}
-                  required
-                  disabled={signUpSuccessMessage}
-                />
-                <span className="invalid-feedback">{errors.firstName}</span>
-              </div>
-              <div className="form-group">
-                <label htmlFor="lastName">
-                  Last Name: <sup>*</sup>
-                </label>
-                <input
-                  type="text"
-                  name="lastName"
-                  className={`form-control ${
-                    errors.lastName ? "is-invalid" : ""
-                  }`}
-                  value={lastName}
-                  onChange={handleChange}
-                  required
-                  disabled={signUpSuccessMessage}
-                />
-                <span className="invalid-feedback">{errors.lastName}</span>
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">
-                  Email: <sup>*</sup>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  className={`form-control ${
-                    errors.email || errors.error ? "is-invalid" : ""
-                  }`}
-                  value={email}
-                  onChange={handleChange}
-                  required
-                  disabled={signUpSuccessMessage}
-                />
-                <span className="invalid-feedback">
-                  {errors.email || errors.error}
-                </span>
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">
-                  Password: <sup>*</sup>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  className={`form-control ${
-                    errors.password ? "is-invalid" : ""
-                  }`}
-                  value={password}
-                  onChange={handleChange}
-                  required
-                  disabled={signUpSuccessMessage}
-                />
-                <span className="invalid-feedback">{errors.password}</span>
-              </div>
-              <div className="form-group">
-                <label htmlFor="confirmPassword">
-                  Confirm Password: <sup>*</sup>
-                </label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  className={`form-control ${
-                    errors.confirmPassword ? "is-invalid" : ""
-                  }`}
-                  value={confirmPassword}
-                  onChange={handleChange}
-                  required
-                  disabled={signUpSuccessMessage}
-                />
-                <span className="invalid-feedback">
-                  {errors.confirmPassword}
-                </span>
-              </div>
-              <div className="row">
-                <div className="col">
+          <LoadingOverlay active={isLoading} spinner={<Spinner />}>
+            <div className="card border-secondary card-body bg-light mt-5 mb-5">
+              <h2 className="text-dark">Create an Account</h2>
+              <p>Please fill out this form to register with us</p>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">
+                    First Name: <sup>*</sup>
+                  </label>
                   <input
-                    type="submit"
-                    value="Register"
-                    className="btn btn-success btn-block"
+                    type="text"
+                    name="firstName"
+                    className={`form-control ${
+                      errors.firstName ? "is-invalid" : ""
+                    }`}
+                    value={firstName}
+                    onChange={handleChange}
+                    required
                     disabled={signUpSuccessMessage}
                   />
+                  <span className="invalid-feedback">{errors.firstName}</span>
                 </div>
-                <div className="col">
-                  <Link className="btn btn-light btn-block" to="/login">
-                    Have an account? Login
-                  </Link>
+                <div className="form-group">
+                  <label htmlFor="lastName">
+                    Last Name: <sup>*</sup>
+                  </label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    className={`form-control ${
+                      errors.lastName ? "is-invalid" : ""
+                    }`}
+                    value={lastName}
+                    onChange={handleChange}
+                    required
+                    disabled={signUpSuccessMessage}
+                  />
+                  <span className="invalid-feedback">{errors.lastName}</span>
                 </div>
-              </div>
-            </form>
-          </div>
+                <div className="form-group">
+                  <label htmlFor="email">
+                    Email: <sup>*</sup>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className={`form-control ${
+                      errors.email || errors.error ? "is-invalid" : ""
+                    }`}
+                    value={email}
+                    onChange={handleChange}
+                    required
+                    disabled={signUpSuccessMessage}
+                  />
+                  <span className="invalid-feedback">
+                    {errors.email || errors.error}
+                  </span>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">
+                    Password: <sup>*</sup>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    className={`form-control ${
+                      errors.password ? "is-invalid" : ""
+                    }`}
+                    value={password}
+                    onChange={handleChange}
+                    required
+                    disabled={signUpSuccessMessage}
+                  />
+                  <span className="invalid-feedback">{errors.password}</span>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmPassword">
+                    Confirm Password: <sup>*</sup>
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    className={`form-control ${
+                      errors.confirmPassword ? "is-invalid" : ""
+                    }`}
+                    value={confirmPassword}
+                    onChange={handleChange}
+                    required
+                    disabled={signUpSuccessMessage}
+                  />
+                  <span className="invalid-feedback">
+                    {errors.confirmPassword}
+                  </span>
+                </div>
+                <div className="row">
+                  <div className="col">
+                    <input
+                      type="submit"
+                      value="Register"
+                      className="btn btn-success btn-block"
+                      disabled={signUpSuccessMessage}
+                    />
+                  </div>
+                  <div className="col">
+                    <Link className="btn btn-light btn-block" to="/login">
+                      Have an account? Login
+                    </Link>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </LoadingOverlay>
         </div>
       </div>
     </div>
