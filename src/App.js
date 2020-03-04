bb20e0f3d59e31e4ac3c5d92d46c65bbbe16a46d
jctiru <jonathanchristiantiru@gmail.com>
2020-03-04 18:11:25 +0800

    Implement sticky footer

--- src/App.js
+++ src/App.js
@@ -31,34 +31,36 @@ const App = ({ checkUserSession, currentUser }) => {
   }, [checkUserSession]);
 
   return (
-    <div>
+    <>
       <Header />
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/cart" component={CartPage} />
-          <Route
-            exact
-            path="/login"
-            render={props =>
-              currentUser ? <Redirect to="/" /> : <LoginPage {...props} />
-            }
-          />
-          <Route
-            exact
-            path="/register"
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <RegisterPage />
-            }
-          />
-          <Route exact path="/novels" component={NovelsShopPage} />
-          <Route exact path="/novels/show/:novelId" component={NovelPage} />
-          <AdminRoute path="/admin" component={AdminPage} />
-        </Switch>
-      </Suspense>
+      <main>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/cart" component={CartPage} />
+            <Route
+              exact
+              path="/login"
+              render={props =>
+                currentUser ? <Redirect to="/" /> : <LoginPage {...props} />
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <RegisterPage />
+              }
+            />
+            <Route exact path="/novels" component={NovelsShopPage} />
+            <Route exact path="/novels/show/:novelId" component={NovelPage} />
+            <AdminRoute path="/admin" component={AdminPage} />
+          </Switch>
+        </Suspense>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
