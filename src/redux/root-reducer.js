62e98b70eb40d58972b382636258f708cff4e02e
jctiru <jonathanchristiantiru@gmail.com>
2020-03-29 16:13:06 +0800

    Use custom storage for redux persist for encrypted local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
-import storage from "redux-persist/lib/storage";
 import createFilter from "redux-persist-transform-filter";
 
-// import customStorage from "./custom-storage";
-
+import customStorage from "./custom-storage";
 import userReducer from "./user/user.reducer";
 import novelReducer from "./novel/novel.reducer";
 import cartReducer from "./cart/cart.reducer";
@@ -16,19 +14,13 @@ const saveUserSubsetFilter = createFilter("user", ["currentUser"]);
 
 const persistConfig = {
   key: "root",
-  storage,
+  storage: customStorage,
   whitelist: ["user", "cart"],
   transforms: [saveUserSubsetFilter],
   stateReconciler: autoMergeLevel2
 };
 
-// const userPersistConfig = {
-//   key: "user",
-//   storage: customStorage
-// };
-
 const rootReducer = combineReducers({
-  //user: persistReducer(userPersistConfig, userReducer),
   user: userReducer,
   novel: novelReducer,
   cart: cartReducer,
