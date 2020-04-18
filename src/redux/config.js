b06ce3a7466fdc7ef8f5fcabc724b6eb09a7bede
jctiru <jonathanchristiantiru@gmail.com>
2020-04-18 17:19:29 +0800

    Make apiUrl in prodConfig https

--- src/redux/config.js
+++ src/redux/config.js
@@ -1,9 +1,9 @@
 const devConfig = {
-  apiUrl: "" // Already using proxy url in package.json
+  apiUrl: "", // Already using proxy url in package.json
 };
 
 const prodConfig = {
-  apiUrl: "http://api.lnshop.jctiru.com"
+  apiUrl: "https://api.lnshop.jctiru.com",
 };
 
 const config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
